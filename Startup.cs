c7dd2ec301ac552de1e2a74a3bb9c67201f0310e
Mark Tiedemann <www.marktiedemann@gmail.com>
2017-04-03 22:29:22 +0200

    Remove service dependency injection

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace dotnet_web_example
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services) { }
-
         public void Configure(IApplicationBuilder app)
         {
             // echo body
